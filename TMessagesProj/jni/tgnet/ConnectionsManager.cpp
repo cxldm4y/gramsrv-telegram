335bc2fb494d17294c803e6ac5a6ecf6469d6400
c0ldych <xzaq36736@gmail.com>
2026-09-10 13:47:51 +0300

    patch server address, add CI build workflow

--- TMessagesProj/jni/tgnet/ConnectionsManager.cpp
+++ TMessagesProj/jni/tgnet/ConnectionsManager.cpp
@@ -1822,7 +1822,7 @@ void ConnectionsManager::initDatacenters() {
     }
 
     std::vector<TcpAddress> localAddresses;
-    localAddresses.push_back(TcpAddress("10.172.61.102", 2398, TcpAddressFlagStatic, ""));
+    localAddresses.push_back(TcpAddress("127.0.0.1", 2398, TcpAddressFlagStatic, ""));
     datacenter->replaceAddresses(localAddresses, 0);
 }
 
